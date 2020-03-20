9a151a40a33cd7cffe6af305f3f1d92c63e6e7e8
André Perez Maselco <andreperezmaselco.developer@gmail.com>
2020-03-20 10:20:19 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
-// Biblioteca padrão C
+// C Library
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 
-// APIs gráficas
+// OpenCL API
 #include <CL/cl.h>
 
 #include "mathematics/set.h"
