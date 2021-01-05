0404e3b0d6701be8fa309b4ed1068760004bf88b
André Perez Maselco <andreperezmaselco.developer@gmail.com>
2021-01-05 16:48:36 -0300

    Argument type according to C11 spec

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "mathematics/set.h"
 #include "OpenCL/platform.h"
 
-int main(const int argc, const char *const *const argv) {
+int main(int argc, char *argv[]) {
   Set platforms;
   allocOpenCLPlatforms(&platforms);
   printOpenCLInfo(&platforms);
