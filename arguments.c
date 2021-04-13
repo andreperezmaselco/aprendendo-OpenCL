8f065e2d91c1f2703e8ca93f1a842f34c529b2e6
André <andreperezmaselco.developer@gmail.com>
2021-04-13 13:41:09 -0300

    Implementar a função parseArguments

--- arguments.c
+++ arguments.c
@@ -10,7 +10,41 @@ static uint16_t arguments = 0b0000011111101111;
 
 void parseArguments(const int argc, char *const argv[]) {
   for (register uint8_t i = 0; i < argc; i++) {
-    // TODO
+    if (strcmp(argv[i], "--platform") == 0 && argv[i + 1] != NULL) {
+      arguments &= 0b0000011111000000;
+      const char *token = strtok(argv[i + 1], ",");
+      do {
+        if (strcmp(token, "name") == 0) {
+          arguments |= 1 << ARGUMENT_PRINT_PLATFORM_NAME;
+        } else if (strcmp(token, "vendor") == 0) {
+          arguments |= 1 << ARGUMENT_PRINT_PLATFORM_VENDOR;
+        } else if (strcmp(token, "profile") == 0) {
+          arguments |= 1 << ARGUMENT_PRINT_PLATFORM_PROFILE;
+        } else if (strcmp(token, "version") == 0) {
+          arguments |= 1 << ARGUMENT_PRINT_PLATFORM_VERSION;
+        } else if (strcmp(token, "extensions") == 0) {
+          arguments |= 1 << ARGUMENT_PRINT_PLATFORM_EXTENSIONS;
+        } else if (strcmp(token, "devices") == 0) {
+          arguments |= 1 << ARGUMENT_PRINT_PLATFORM_DEVICES;
+        }
+      } while ((token = strtok(NULL, ",")) != NULL);
+    } else if (strcmp(argv[i], "--device") == 0 && argv[i + 1] != NULL) {
+      arguments &= 0b0000000000111111;
+      const char *token = strtok(argv[i + 1], ",");
+      do {
+        if (strcmp(token, "name") == 0) {
+          arguments |= 1 << ARGUMENT_PRINT_DEVICE_NAME;
+        } else if (strcmp(token, "vendor") == 0) {
+          arguments |= 1 << ARGUMENT_PRINT_DEVICE_VENDOR;
+        } else if (strcmp(token, "profile") == 0) {
+          arguments |= 1 << ARGUMENT_PRINT_DEVICE_PROFILE;
+        } else if (strcmp(token, "version") == 0) {
+          arguments |= 1 << ARGUMENT_PRINT_DEVICE_VERSION;
+        } else if (strcmp(token, "type") == 0) {
+          arguments |= 1 << ARGUMENT_PRINT_DEVICE_TYPE;
+        }
+      } while ((token = strtok(NULL, ",")) != NULL);
+    }
   }
 }
 
